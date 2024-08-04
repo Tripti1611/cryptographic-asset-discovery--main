0c163c2e7de7553d704f832f18cf0f6ce5655b89
Tripti Sharma <70108118+Tripti1611@users.noreply.github.com>
2024-08-04 18:53:13 +0530

    Create apiconstants.js

---
+++ src/common/utils/apiconstants.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    SYSTEMS: "/api/v1/server/all",
+    SYSTEMS_ADD: "/api/v1/server/create",
+    SYSTEM_ASSET: "/api/v1/certificates",
+    SYSTEM_DISCOVER: "/api/v1/certificates/find",
+    SYTEM_KMIP: "/api/v1/kmip/key/find"
+}
